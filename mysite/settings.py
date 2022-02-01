33d5675071f51f1c74c5389fc529d27f9e483ece
Saif Ali <37900854+saifali3251@users.noreply.github.com>
2022-02-01 12:47:22 +0530

    Update settings.py
    Added sqlite3 as default DB

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER' : 'postgres',
-        'PASSWORD' : '3251',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
